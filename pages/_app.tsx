76f1d524fd687df20a19cffec1262e87d70583aa
JoseLAmador <josel.amador.s@gmail.com>
2022-06-24 17:15:05 -0500

    feat: add support to layouts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app"
+import type { NextPage } from "next";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: React.ReactElement) => JSX.Element;
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(<Component {...pageProps} />)
 }
 
 export default MyApp

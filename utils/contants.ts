9b7fca8a8b9ae5253c63ed4e6e10d7d7e88ea0f9
JoseLAmador <josel.amador.s@gmail.com>
2022-06-24 17:32:44 -0500

    feat: add slug property to links data

--- utils/contants.ts
+++ utils/contants.ts
@@ -2,6 +2,7 @@ export const HOME_LINKS = [
   {
     to: "/search-user",
     label: "Github user",
+    slug: "user",
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor obcaecati reiciendis ullam,
     ipsam a rem sunt facere impedit culpa est voluptates eos eligendi illum praesentium
     voluptatibus amet. Obcaecati, autem quo.`
@@ -9,6 +10,7 @@ export const HOME_LINKS = [
   {
     to: "/search-repo",
     label: "Github repository",
+    slug: "repository",
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor obcaecati reiciendis ullam,
     ipsam a rem sunt facere impedit culpa est voluptates eos eligendi illum praesentium
     voluptatibus amet. Obcaecati, autem quo.`

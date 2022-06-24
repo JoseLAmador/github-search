73689e7c0d908ed9c7ca99576e6353539e17b1bc
JoseLAmador <josel.amador.s@gmail.com>
2022-06-24 17:26:48 -0500

    feat: add home links data

---
+++ utils/contants.ts
@@ -0,0 +1,16 @@
+export const HOME_LINKS = [
+  {
+    to: "/search-user",
+    label: "Github user",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor obcaecati reiciendis ullam,
+    ipsam a rem sunt facere impedit culpa est voluptates eos eligendi illum praesentium
+    voluptatibus amet. Obcaecati, autem quo.`
+  },
+  {
+    to: "/search-repo",
+    label: "Github repository",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor obcaecati reiciendis ullam,
+    ipsam a rem sunt facere impedit culpa est voluptates eos eligendi illum praesentium
+    voluptatibus amet. Obcaecati, autem quo.`
+  }
+]

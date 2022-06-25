848d3f686e37210f6e4cf0c1051d70d704d31fdd
JoseLAmador <josel.amador.s@gmail.com>
2022-06-25 13:01:36 -0500

    feat: use next image instead img html tag

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import type { GithubUser } from "types/github-user";
 
 export default function UserCard(props: Pick<GithubUser, "avatar_url" | "login" | "html_url">) {
@@ -12,9 +13,11 @@ export default function UserCard(props: Pick<GithubUser, "avatar_url" | "login" 
       <a target="_blank">
         <div className="p-4 bg-white rounded shadow-xl hover:shadow-2xl flex items-center flex-col space-y-2">
           <div className="shrink-0">
-            <img
+            <Image
               className="h-32 w-32 rounded"
               src={avatar_url}
+              height={128}
+              width={128}
               alt={login} />
           </div>
           <h3 className="text-xl font-medium text-primary-500">{login}</h3>

c48f11946238c8dd1d5d220795f1e54dbec3f800
JoseLAmador <josel.amador.s@gmail.com>
2022-06-25 13:43:42 -0500

    style: add focus styles

--- components/Input.tsx
+++ components/Input.tsx
@@ -8,7 +8,7 @@ const Input = forwardRef((props: InputHTMLAttributes<HTMLInputElement>, ref: Ref
   return (
     <input
       ref={ref}
-      className="block w-full border border-gray-300 rounded-md px-4 py-2 text-sm placeholder-gray-500 focus:outline-none focus:text-gray-900"
+      className="block w-full border border-gray-300 rounded-md px-4 py-2 text-sm placeholder-gray-500 focus:outline-none focus:text-gray-900 focus:ring-primary-500 focus:border-primary-500"
       {...rest}
       />
   )

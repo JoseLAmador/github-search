6cf509d7fa2d6186cff96f95d63f6443aba451b6
JoseLAmador <josel.amador.s@gmail.com>
2022-06-25 13:01:56 -0500

    fix: use common types

--- hooks/useGithubRepos.ts
+++ hooks/useGithubRepos.ts
@@ -3,21 +3,16 @@ import fetcher from "@/utils/fetcher";
 import { BASE_URL } from "@/utils/contants";
 import type { UseQueryOptions } from "react-query";
 import type { GithubRepo } from "types/github-repo";
+import type { Results } from "types/common";
 
 const headers = new Headers({
   "Content-Type": "application/json;charset=UTF-8",
 });
 
-type GithubReposDataResponse = {
-  total_count: number;
-  incomplete_results: boolean;
-  items: Array<GithubRepo>;
-}
-
 /**
  * TODO: Validate options type
  */
-export default function useGithubRepos(search?: string, options: Omit<UseQueryOptions<GithubReposDataResponse>, "githubRepos"> = {}) {
+export default function useGithubRepos(search?: string, options: Omit<UseQueryOptions<Results<GithubRepo>>, "githubRepos"> = {}) {
   const key = "githubRepos";
 
   return useQuery(
@@ -26,7 +21,7 @@ export default function useGithubRepos(search?: string, options: Omit<UseQueryOp
       search || ""
     ],
     async () => {
-      const data = await fetcher<GithubReposDataResponse>(`${BASE_URL}/search/repositories?q=${search}&per_page=9`, headers);
+      const data = await fetcher<Results<GithubRepo>>(`${BASE_URL}/search/repositories?q=${search}&per_page=9`, headers);
       return data;
     },
     options
--- hooks/useGithubUsers.ts
+++ hooks/useGithubUsers.ts
@@ -3,21 +3,16 @@ import fetcher from "@/utils/fetcher";
 import { BASE_URL } from "@/utils/contants";
 import type { UseQueryOptions } from "react-query";
 import type { GithubUser } from "types/github-user";
+import type { Results } from "types/common";
 
 const headers = new Headers({
   "Content-Type": "application/json;charset=UTF-8",
 });
 
-type GithubUsersDataResponse = {
-  total_count: number;
-  incomplete_results: boolean;
-  items: Array<GithubUser>;
-}
-
 /**
  * TODO: Validate options type
  */
-export default function useGithubUsers(search?: string, options: Omit<UseQueryOptions<GithubUsersDataResponse>, "githubUsers"> = {}) {
+export default function useGithubUsers(search?: string, options: Omit<UseQueryOptions<Results<GithubUser>>, "githubUsers"> = {}) {
   const key = "githubUsers";
 
   return useQuery(
@@ -26,7 +21,7 @@ export default function useGithubUsers(search?: string, options: Omit<UseQueryOp
       search || ""
     ],
     async () => {
-      const data = await fetcher<GithubUsersDataResponse>(`${BASE_URL}/search/users?q=${search}&per_page=9`, headers);
+      const data = await fetcher<Results<GithubUser>>(`${BASE_URL}/search/users?q=${search}&per_page=9`, headers);
       return data;
     },
     options

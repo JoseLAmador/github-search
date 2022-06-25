7b3326457abeffaa475de416a7b29bf140068091
JoseLAmador <josel.amador.s@gmail.com>
2022-06-25 10:58:54 -0500

    feat: add react query config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,6 @@
+import { useRef } from "react";
+import { Hydrate, QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools"
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app"
 import type { NextPage } from "next";
@@ -11,9 +14,18 @@ type AppPropsWithLayout = AppProps & {
 }
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const queryClient = useRef(new QueryClient());
+
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(<Component {...pageProps} />)
+  return getLayout(
+    <QueryClientProvider client={queryClient?.current}>
+      <Hydrate state={pageProps?.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools/>
+    </QueryClientProvider>
+  )
 }
 
-export default MyApp
+export default MyApp;
